7645a8fd984e78b3bc1a1544eadefe9028fc8145
Stommes <Pete.Stommes@wolterskluwer.com>
2017-11-10 16:07:17 -0600

    fixed component style encapsulation

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,20 +1,17 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { UserService } from '../shared/user.service';
 import { IUser } from '../shared/user';
-import { userInfo } from 'os';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class UserListComponent implements OnInit {
 
   users: IUser[];
   filteredUsers: IUser[];
-
-
   errorMessage: string;
 
   _filterString: string;
--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -5,7 +5,7 @@ import { IUser } from '../shared/user';
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class UserComponent implements OnInit {
 
